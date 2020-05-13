81f840033ab195048d390afa31a2b7de8a7da74d
burrito1 <gon.rivero10@gmail.com>
2020-05-13 17:00:15 -0300

    UE4 Project files, ready to be Build as Window Server

---
+++ IoniasCall/Source/IoniasCall/Private/IoniasCall.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IoniasCall.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IoniasCall, "IoniasCall" );
+ 
---
+++ IoniasCall/Source/IoniasCall/Public/IoniasCallGameMode.h
@@ -0,0 +1,84 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameLiftServerSDK.h"
+#include "GameFramework/GameModeBase.h"
+#include "IoniasCallGameMode.generated.h"
+
+USTRUCT()
+struct FStartGameSessionState
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+		bool Status;
+
+	FStartGameSessionState() {
+		Status = false;
+	}
+};
+
+USTRUCT()
+struct FUpdateGameSessionState
+{
+	GENERATED_BODY();
+
+	FUpdateGameSessionState() {
+
+	}
+};
+
+USTRUCT()
+struct FProcessTerminateState
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+		bool Status;
+
+	long TerminationTime;
+
+	FProcessTerminateState() {
+		Status = false;
+	}
+};
+
+USTRUCT()
+struct FHealthCheckState
+{
+	GENERATED_BODY();
+
+	UPROPERTY()
+		bool Status;
+
+	FHealthCheckState() {
+		Status = false;
+	}
+};
+
+UCLASS(minimalapi)
+class AIoniasCallGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIoniasCallGameMode();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+		FStartGameSessionState StartGameSessionState;
+
+	UPROPERTY()
+		FUpdateGameSessionState UpdateGameSessionState;
+
+	UPROPERTY()
+		FProcessTerminateState ProcessTerminateState;
+
+	UPROPERTY()
+		FHealthCheckState HealthCheckState;
+};
